11fd551d6605599121eba2cfc7028d3208b70c48
Abby Corceiro <abbycorceiro@gmail.com>
2025-06-03 18:56:51 -0300

    Added Model

---
+++ ZapazAPI/ZapazAPI/Context/ZapaDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZapazAPI.Models;
+
+namespace ZapazAPI.Context
+{
+    public class ZapaDBContext : DbContext
+    {
+        public ZapaDBContext(DbContextOptions<ZapaDBContext>options):base(options) { }
+        public DbSet<Zapa> Zapas { get; set; }
+    }
+}
---
+++ ZapazAPI/ZapazAPI/Models/Zapa.cs
@@ -0,0 +1,13 @@
+﻿namespace ZapazAPI.Models
+{
+    public class Zapa
+    {
+        public required string Id { get; set; }
+        public required string Marca { get; set; }
+        public required string Modelo { get; set; }
+        public required int Talle { get; set; }
+        public required string Color { get; set; }
+        public required bool Disponible { get; set; }
+        public required int Stock { get; set; }
+    }
+}
